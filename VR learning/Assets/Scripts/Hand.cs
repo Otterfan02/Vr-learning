abb4a037b1f9481e47cf9a33071834db9a25d3cb
RonaldHR <ronaldteigen@hotmail.no>
2021-09-30 11:02:42 +0200

    Hand physics beging

--- VR learning/Assets/Scripts/Hand.cs
+++ VR learning/Assets/Scripts/Hand.cs
@@ -6,32 +6,58 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Hand : MonoBehaviour
 {
+//Animation variables
     private Animator animator;
-
-    public float speed;
-
+    public float animationSpeed;
     private float gripTarget;
     private float triggerTarget;
-
     private float gripCurrent;
     private float triggerCurrent;
     private string animatorGripParam = "Grip";
     private string animatorTriggerParam = "Trigger";
-
     private SkinnedMeshRenderer mesh;
     
+    
+    //Physics Movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+    private Transform followTarget;
+    private Rigidbody body;
     
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         mesh.GetComponentInChildren<SkinnedMeshRenderer>();
+
+        //Physics movement
+        followTarget = followObject.transform;
+        body = GetComponent<Rigidbody>();
+        body.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        body.interpolation = RigidbodyInterpolation.Interpolate;
+        body.mass = 20f;
+        
+        //Teleport hands
+        body.position = followTarget.position;
+        body.rotation = followTarget.rotation;
+        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         AnimateHand();
+
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        var distance = Vector3.Distance(followTarget.position, transform.position);
+        body.velocity = (followTarget.position - transform.position).normalized * (followSpeed * distance);
     }
 
     public void SetGrip(float readValue)
@@ -48,11 +74,11 @@ public class Hand : MonoBehaviour
     {
         if (gripCurrent != gripTarget)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorGripParam, gripCurrent);
         }        if (triggerCurrent != triggerTarget)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * animationSpeed);
             animator.SetFloat(animatorTriggerParam, triggerCurrent);
         }
     }
