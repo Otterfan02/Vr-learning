b86af0fa385ec7eb63fbfff68090c2c53533f850
RonaldHR <ronaldteigen@hotmail.no>
2021-09-30 09:51:24 +0200

    Initial Commit

---
+++ VR learning/Assets/Scripts/Hand.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    private Animator animator;
+
+    public float speed;
+
+    private float gripTarget;
+    private float triggerTarget;
+
+    private float gripCurrent;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+
+    private SkinnedMeshRenderer mesh;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mesh.GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    public void SetGrip(float readValue)
+    {
+        gripTarget = readValue;
+    }
+
+    public void SetTrigger(float readValue)
+    {
+        triggerTarget = readValue;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        }        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        mesh.enabled = !mesh.enabled;
+    }
+}
